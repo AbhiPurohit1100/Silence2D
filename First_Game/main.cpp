2fcb90bd25c3d4974a5c33884c1b2e28ea01ad6b
Abhinav Purohit <117670205+AbhiPurohit1100@users.noreply.github.com>
2024-07-27 12:51:37 +0530

    movement and cat

--- First_Game/main.cpp
+++ First_Game/main.cpp
@@ -1,43 +1,77 @@
 #include <SFML/Graphics.hpp>
+#include<iostream>
+#include <chrono>
+#include <thread>
+using namespace std;
 using namespace sf;
 int main()
 {
 	ContextSettings set;
-	RenderWindow window(VideoMode(800,600), "Game", Style::Default, set); 
+	RenderWindow window(VideoMode(800, 600), "Game", Style::Default, set);
 
-	
-	set.antialiasingLevel= 8;
 
-	CircleShape circle1(50);
-	circle1.setFillColor(Color::White);
-	circle1.setPosition(Vector2f(350, 250));
-	circle1.setOutlineThickness(10);
-	circle1.setOutlineColor(Color::Black);
+	set.antialiasingLevel = 8;
 
-	RectangleShape rec1(Vector2f(100, 50));
-	rec1.setFillColor(Color::Green);
-	rec1.setOutlineColor(Color::Black);
-	rec1.setOutlineThickness(10);
-	rec1.setPosition(Vector2f(100, 100));
 
-	
+	Texture player1text;
+	Sprite player1sprite;
+
+
+	if (player1text.loadFromFile("Assets/texture/walkk.png")) {
+
+		std::cout << "player loaded" << std::endl;
+		player1sprite.setTexture(player1text);
+		int Xindex = 0;
+		int Yindex = 0;
+
+		player1sprite.setTextureRect(IntRect(Xindex * 64, Yindex * 64, 64, 64));
+
+		player1sprite.scale(Vector2f(2, 2));
+	}
+	else {
+		std::cout << "failed to load" << std::endl;
+	}
+
+
 	//main game loop
 
-	while (window.isOpen()) {  
+	while (window.isOpen()) {
 
-		Event event1;  
+		Event event1;
 
 		while (window.pollEvent(event1)) {
-		//-------------------------------------UPDATE-----------------------------------------
+			
 
-			if (event1.type == Event::Closed)  
+			if (event1.type == Event::Closed)
 				window.close();
 		}
-		//-------------------------------------DRAW-----------------------------------------------
-		window.clear(Color::Yellow);
-		
-		window.draw(circle1);
-		window.draw(rec1);
+
+		Vector2f Cposition = player1sprite.getPosition();
+		if (Keyboard::isKeyPressed(Keyboard::D)) {
+
+			
+			player1sprite.setPosition(Cposition + Vector2f(0.3, 0));
+		}
+		if (Keyboard::isKeyPressed(Keyboard::A)) {
+
+			
+			player1sprite.setPosition(Cposition + Vector2f(-0.3, 0));
+		}
+		if (Keyboard::isKeyPressed(Keyboard::W)) {
+
+			player1sprite.setPosition(Cposition + Vector2f(0, -0.3));
+		}
+		if (Keyboard::isKeyPressed(Keyboard::S)){
+
+			
+			player1sprite.setPosition(Cposition + Vector2f(0, 0.3));
+		}
+
+
+
+		window.clear(Color::Black);
+		window.draw(player1sprite);
+
 		window.display();
 
 
@@ -45,7 +79,7 @@ int main()
 
 
 	}
-	 
+
 
 
 	return 0;
